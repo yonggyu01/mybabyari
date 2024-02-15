ac6ec4cb8a46f755048718f239bd97c8136a71f4
yonggyu <yonggyu01@naver.com>
2024-02-15 16:00:05 +0900

    리베이스 완료

--- src/views/MainGame.vue
+++ src/views/MainGame.vue
@@ -1,7 +1,6 @@
 <template>
     <div class="backblack">
         <div class="overwrapbox">
-<<<<<<< HEAD
              <div class="cube-wrapper" :style="cubewrapper">
              <div class="cube-front"><img src="../assets/img/zombie1.png" alt=""></div>
              <!-- <div class="cube-front-door"><img src="img/door.png" alt=""></div> -->
@@ -103,93 +102,6 @@ export default {
     --R: calc(pi * 2 / 360); /* pi is 3.141592653589793 */
     --angle: 90;
 }
-=======
-            <div class="cube-wrapper" :style="cubewrapper">
-                <div class="cube-front"><img src="../assets/img/zombie1.png" alt=""></div>
-                <!-- <div class="cube-front-door"><img src="img/door.png" alt=""></div> -->
-                <div class="cube-front1"><img src="../assets/img/zombie1.png" alt=""></div>
-                <div class="cube-frontfull"></div>
-                <div class="cube-back" :style="gunaa"><img src="../assets/img/gun.png" alt=""></div>
-                <div class="cube-top"></div>
-                <div class="cube-bottom"></div>
-                <div class="cube-left"></div>
-                <div class="cube-right"></div>
-            </div>
-        </div>
-        <div class="boxwrap">
-            <div class="upbox">
-                <a href="#none" id="upbtn" @click="upbtn">Up</a>
-                <a href="#none" id="turnbtn">TURN</a>
-            </div>
-            <div class="threebox">
-                <a href="#none" id="leftbtn" @click="left">Left</a>
-                <a href="#none" id="downbtn" @click="downbtn">Down</a>
-                <a href="#none" id="rightbtn" @click="right">Right</a>
-            </div>
-        </div>
-    </div>
-</template>
-<script>
-
-export default {
-    name: 'gamepage',
-    methods: {
-        upbtn() {
-            this.moving += 50
-            console.log(this.gunaa, this.eta)
-        },
-        downbtn() {
-            this.moving -= 50
-        },
-        left() {
-            if (this.moving < 6700) {
-                if (this.leftright < 70) {
-                    this.leftright += 7
-                }
-            }
-        },
-        right() {
-            if (this.moving < 6700) {
-                if (this.leftright > -60) {
-                    this.leftright -= 7
-                }
-            }
-        }
-    },
-    data() {
-        return {
-            leftright: 0,
-            moving: 0,
-        }
-    },
-    computed: {
-        cubewrapper() {
-            return `transform: perspective(700px) rotateX(360deg) rotateY(360deg) translate3d(${this.leftright}px, 0px, ${this.moving}px);transform-origin: 50% 50% ${-this.moving}px`
-        },
-        gunaa() {
-            return `transform = rotateY(180deg) translate3d(${this.leftright - 60}px, 67px, ${-500 + this.moving}px)  matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 3)`
-        }
-
-    }
-}
-</script>
-<style scoped>
-.backblack {
-    background-color: black;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 100vw;
-    height: 100vh;
-}
-
-.overwrapbox {
-    --R: calc(pi * 2 / 360);
-    /* pi is 3.141592653589793 */
-    --angle: 90;
-}
-
->>>>>>> 9ba673e (로그인버튼 수정함)
 .cube-wrapper {
     --applyAngle: calc(var(--angle) * var(--R));
     width: 100vw;
@@ -199,27 +111,16 @@ export default {
     transform: perspective(700px) rotateX(360deg) rotateY(360deg) translate3d(0px, 0px, 0px);
     position: relative;
     transition: 0.3s;
-<<<<<<< HEAD
 /*     
     left: calc(50%);
     top: calc(50%); */
 }
 .cube-wrapper > div {
 	position: absolute;
-=======
-    /*     
-    left: calc(50%);
-    top: calc(50%); */
-}
-
-.cube-wrapper>div {
-    position: absolute;
->>>>>>> 9ba673e (로그인버튼 수정함)
     width: 100vw;
     height: 100vh;
     text-align: center;
     /* line-height: 100px; */
-<<<<<<< HEAD
    	font-weight: bold;
     font-size: 20px;
 
@@ -247,47 +148,11 @@ width: 100%;
  
     transform: translate3d(90px, 49px,-4666px) matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 4);
 }
-=======
-    font-weight: bold;
-    font-size: 20px;
-
-}
-
-.cube-frontfull {
-    background-color: white;
-
-    transform: translate3d(0, 49px, -5666px) matrix3d(150, 0, 0, 0, 0, 155, 0, 0, 0, 0, 1, 0, 0, 0, 0, 4);
-}
-
-.cube-front {
-    transform: translate3d(0, 49px, -1666px) matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 4);
-}
-
-.cube-front-door {
-    transform: translate3d(0, 49px, -1666px) matrix3d(2, 0, 0, 0, 0, 2, 0, 0, 0, 0, 1, 0, 0, 0, 0, 7);
-    display: flex;
-    align-items: center;
-    justify-content: center;
-}
-
-.cube-front-door img {
-    width: 100%;
-
-}
-
-.cube-front1 {
-    /* background-color:white; */
-
-    transform: translate3d(90px, 49px, -4666px) matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 4);
-}
-
->>>>>>> 9ba673e (로그인버튼 수정함)
 /* iframe{
     width: 100vw;
     height: 100vh;
 } */
 .cube-back {
-<<<<<<< HEAD
    
     transition: 1 all;
     /* background-image:url(img/gun.png); */
@@ -295,22 +160,11 @@ width: 100%;
     transform: rotateY(180deg) translate3d(-60px, 67px, -500px)  matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 3)
 }
 .cube-back img{
-=======
-
-    transition: 1 all;
-    /* background-image:url(img/gun.png); */
-    background-repeat: no-repeat;
-    transform: rotateY(180deg) translate3d(-60px, 67px, -500px) matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 3)
-}
-
-.cube-back img {
->>>>>>> 9ba673e (로그인버튼 수정함)
     width: 300px;
     transform: rotateX(-45deg);
     object-fit: cover;
 
 }
-<<<<<<< HEAD
 .cube-top {
     background-color : rgb(121, 121, 121);
     transform: rotateX(89deg) translate3d(0, 0, 120px) matrix(-1, 0, 0, 20, 0, 0);
@@ -345,38 +199,10 @@ width: 100%;
  
 
 .overwrapbox{
-=======
-
-.cube-top {
-    background-color: rgb(121, 121, 121);
-    transform: rotateX(89deg) translate3d(0, 0, 120px) matrix(-1, 0, 0, 20, 0, 0);
-}
-
-.cube-bottom {
-    background-color: rgb(102, 16, 16);
-    transform: rotateX(-88deg) translate3d(0, 0, 120px) matrix(-1, 0, 0, 20, 0, 0);
-}
-
-.cube-left {
-    background-image: url(../assets/img/Untitled-1.png);
-    transform: rotateY(-89deg) translate3d(0, 0, 150px) matrix(-12, 0, 0, 1, 0, 0);
-
-}
-
-.cube-right {
-    background-image: url(../assets/img/Untitled-1.png);
-    transform: rotateY(89deg) translate3d(0, 0, 150px) matrix(-12, 0, 0, 1, 0, 0);
-    ;
-}
-
-
-.overwrapbox {
->>>>>>> 9ba673e (로그인버튼 수정함)
     width: 100vw;
     height: 100vh;
     overflow: hidden;
 }
-<<<<<<< HEAD
 .modalbox{
     display: flex;
     flex-direction: row;
@@ -389,30 +215,12 @@ a{
             color:black
         }
         .boxwrap{
-=======
-
-.modalbox {
-    display: flex;
-    flex-direction: row;
-    position: absolute;
-    top: 50%;
-    left: calc(50%)
-}
-
-a {
-    text-decoration: none;
-    color: black
-}
-
-.boxwrap {
->>>>>>> 9ba673e (로그인버튼 수정함)
     display: flex;
     flex-direction: column;
     width: 300px;
     /* height: 220px; */
     position: absolute;
     bottom: calc(25vh - 80px);
-<<<<<<< HEAD
     left : -40px;
     align-items: center;
     gap: 5px;   
@@ -421,21 +229,6 @@ a {
 
 .threebox{display: flex; gap : 10px; box-sizing: border-box;} 
 .boxwrap a{
-=======
-    left: -40px;
-    align-items: center;
-    gap: 5px;
-    box-sizing: border-box;
-}
-
-.threebox {
-    display: flex;
-    gap: 10px;
-    box-sizing: border-box;
-}
-
-.boxwrap a {
->>>>>>> 9ba673e (로그인버튼 수정함)
     display: flex;
     justify-content: center;
     align-items: center;
@@ -443,7 +236,6 @@ a {
     border: 1px solid black;
     height: 3em;
     background-color: bisque;
-<<<<<<< HEAD
  
     
 }
@@ -451,27 +243,11 @@ a {
     display: flex;
 }
 #turnbtn{
-=======
-
-
-}
-
-.upbox {
-    display: flex;
-}
-
-#turnbtn {
->>>>>>> 9ba673e (로그인버튼 수정함)
     position: absolute;
     width: 50px;
     height: 50px;
     border-radius: 50%;
-<<<<<<< HEAD
     left : 65px;
 }
 
 </style>
-=======
-    left: 65px;
-}</style>
->>>>>>> 9ba673e (로그인버튼 수정함)
